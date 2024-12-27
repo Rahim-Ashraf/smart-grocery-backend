6a6d6d09d40d5dc68c604ed4175e020bbd9d6118
unknown <rahim11500000@gmail.com>
2024-12-27 19:49:10 +0600

    make user APIs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
     @Get()
-    getUser() {
-        return []
+    getUsers() {
+        return this.usersService.getUsers()
+    }
+
+    @Get(':id')
+    getUser(@Param('id') id: string) {
+        return this.usersService.getUser(id)
+    }
+
+    @Post()
+    createUser() {
+        return this.usersService.createUser()
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id') id: string) {
+        return this.usersService.updateUser(id)
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string) {
+        return this.usersService.deleteUser(id)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    getUsers() {
+        return ['dkfjs']
+    }
+    getUser(id: string) {
+        return { id }
+    }
+    createUser() {
+        return {
+            inserted: true,
+            message: `New user created`
+        }
+    }
+    updateUser(id: string) {
+        return {
+            updated: true,
+            message: `User ${id} have been updated`
+        }
+    }
+    deleteUser(id: string) {
+        return {
+            deleteCount: 1,
+            message: `User ${id} have been deleted`
+        }
+    }
+}
